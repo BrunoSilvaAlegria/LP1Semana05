20700d5356b67af1cbe16d026ddcefe707c6bb7b
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-03-20 21:38:52 +0000

    ex 1, started and paused cron2, stops both crons

--- Chronos/Program.cs
+++ Chronos/Program.cs
@@ -12,11 +12,21 @@ namespace Chronos
             Stopwatch cron1 = new Stopwatch();
             Stopwatch cron2 = new Stopwatch();
 
-            //Começar a contagem dos primeiro cronómetro
+            //Começar a contagem do primeiro cronómetro
             cron1.Start();
 
-            //Pausar o mesmo cronómetro
+            //Pausar o cronómetro 1
             cron1.Thread.Sleep(600);
+
+            //Começar a contagem do segundo cronómetro
+            cron2.Start();
+
+            //Pausar o cronómetro 2
+            cron2.Thread.Sleep(200);
+
+            //Parar a contagem dos cronómetros
+            cron1.Stop();
+            cron2.Stop();
 
         }
     }
