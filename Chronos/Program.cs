72b2f6369f6d86ddd92d5f47e6ccd7335c11f2ce
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-03-20 22:10:59 +0000

    ex 1, corrected Theard error, results on screen, tested and funcional

--- Chronos/Program.cs
+++ Chronos/Program.cs
@@ -16,17 +16,30 @@ namespace Chronos
             cron1.Start();
 
             //Pausar o cronómetro 1
-            cron1.Thread.Sleep(600);
+            Thread.Sleep(600);
 
             //Começar a contagem do segundo cronómetro
             cron2.Start();
 
             //Pausar o cronómetro 2
-            cron2.Thread.Sleep(200);
+            Thread.Sleep(200);
 
             //Parar a contagem dos cronómetros
             cron1.Stop();
             cron2.Stop();
+
+            //Obtém o tempo total decorrido, medido pelas instâncias, 
+            //em milissegundos.
+            long mill1 = cron1.ElapsedMilliseconds;
+            long mill2 = cron2.ElapsedMilliseconds;
+
+            //Passar os milissegundos para segundos
+            double seg1 = mill1 / 1000.0;
+            double seg2 = mill2 / 1000.0;
+
+            //Mostra no ecrã o tempo decorrido em cada cronómetro, 
+            //em segundos, com três casas decimais
+            Console.WriteLine($"Cronómetro 1:{seg1:f3}|Cronómetro 2:{seg2:f3}");
 
         }
     }
