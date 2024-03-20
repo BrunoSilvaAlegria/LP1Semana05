f5b3e795fa6637c083f3b76b3cef1cc12198efc8
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-03-20 20:32:14 +0000

    ex 1, added stopwatches, started and paused 1

---
+++ Chronos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Chronos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Criar e instanciar os dois cronómetros nesta classe
+            Stopwatch cron1 = new Stopwatch();
+            Stopwatch cron2 = new Stopwatch();
+
+            //Começar a contagem dos primeiro cronómetro
+            cron1.Start();
+
+            //Pausar o mesmo cronómetro
+            cron1.Thread.Sleep(600);
+
+        }
+    }
+}
