9ecdc1824fa008a3485d79175e249e6546cc108d
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-03-21 01:09:17 +0000

    ex 5, added sum dice values and XML documentation

--- RanDice/Program.cs
+++ RanDice/Program.cs
@@ -3,7 +3,13 @@
 namespace RanDice
 {
     class Program
-    {
+    {   
+        /// <summary>
+        /// O utilizador escolhe o número de dados (n) e a seed (s) que deseja.
+        /// Depois, é escolhido um valor entre 1 e 6 para a rodagem de cada dado
+        /// e soma-se todos os valores que saírem, que é apresentado no ecrã.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             if (args.Length == 2)
@@ -11,22 +17,23 @@ namespace RanDice
                 int n = int.Parse(args[0]); //Número de dados
                 int s = int.Parse(args[1]); //Seed
 
+                int soma = 0; //Inicializar a variável da soma dos números
+
                 Random dados = new Random(s); //Seed s definida
-                
+
                 for (int i = 0; i < n; i++) //"Roda" todos os dados
                 {
                     //Escolhe um número entre 1 e 6 para cada dado
                     int nums = dados.Next(1,7);
+                    soma += nums; //Soma dos valores escolhidos
                     
-                }
-                    
+                }  
+                Console.WriteLine($"A soma dos valores que calharam é: {soma}");     
             }
             else
             {
                 Console.WriteLine("This program takes 2 arguments!");
-            }
-            
-            
+            } 
         }
     }
 }
