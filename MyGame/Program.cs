6e92504dcbf9ed46fe57a31eaf8a4c7eb66da3a7
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-03-21 22:28:01 +0000

    ex 6, added class Enemy, build sucess

---
+++ MyGame/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyGame
+{
+    public class Enemy
+    {
+        
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
