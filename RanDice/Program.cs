eb5d64c006b953c3e01dba860aeb1e52d2d64910
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-03-21 00:05:06 +0000

    ex 5, added argument detection

---
+++ RanDice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RanDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 2)
+            {
+                int n = args[0]; //Número de dados
+                int s = args[1]; //Seed
+
+                
+            }
+            else
+            {
+                Console.WriteLine("This program only takes 2 arguments!");
+            }
+            
+            
+        }
+    }
+}
