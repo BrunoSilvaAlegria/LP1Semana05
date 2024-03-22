395c7a266f71d191e79e6bb822c0951e84a18f70
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-03-22 22:20:41 +0000

    ex extra, added getter methods and method SetEnemyInRoom

--- MyGameFriday/GameLevel.cs
+++ MyGameFriday/GameLevel.cs
@@ -9,13 +9,23 @@ namespace MyGameFriday
     {
         //Inicilizar as variáveis
         private Difficulty lv;
-        private int RoomsNum = 0; 
+        private int RoomsNum = 0;
+        private int EnemNum = 0;
         private string[] rms;
         public GameLevel(int RoomsNum, Difficulty lv)
         {
-            this.RoomsNum = RoomsNum; //Substitui o nº original do room pelo escolhido
+            this.RoomsNum = RoomsNum; //Substitui o nº de salas pelo escolhido
             this.lv = lv; //Substitui a dificuldade original pelo escolhido
             rms = new string[RoomsNum];
+        }
+        public Difficulty GetDifficulty() => lv; //Devolve a dificuldade
+        public int GetNumRooms() => RoomsNum; //Devolve o número de salas
+        public int GetNumEnemies() => EnemNum; //Devolve o número de inimigos
+        public void SetEnemyInRoom(int room, Enemy enemy) 
+        {
+            //Coloca um inimigo específico na sala escolhida 
+            rooms[room] = enemy.GetName();
+            EnemNum++;
         }
     }
 }
